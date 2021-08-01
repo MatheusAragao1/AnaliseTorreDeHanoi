5097d4cccf0a6972e15b74fe219e74ae65787f89
Luiz Gustavo Pereira <luizgustavopereira@id.uff.br>
2021-08-01 01:23:18 -0300

    Método_IterativoBit_a_bit

---
+++ Recursivo/cmake-build-debug/CMakeFiles/AnaliseTorreDeHanoi.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AnaliseTorreDeHanoi.exe"
+  "AnaliseTorreDeHanoi.pdb"
+  "CMakeFiles/AnaliseTorreDeHanoi.dir/torreDeHanoiRecursiva.c.o"
+  "libAnaliseTorreDeHanoi.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AnaliseTorreDeHanoi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Recursivo/torreDeHanoiRecursiva.c
+++ Recursivo/torreDeHanoiRecursiva.c
@@ -87,6 +87,79 @@ void moverDiscos(int pinoOrigem[], int pinoDestino[])
     }
 }
 
+void torreDeHanoiIterativa_metodo_xxx(int numDisk, int printar, int *seq, FILE *arq)
+{
+    //https://www.irjet.net/archives/V8/i3/IRJET-V8I352.pdf
+    int k, tot;
+    int de, para;
+    int even;
+    if (numDisk % 2 == 0) {
+        even = 1; //é par
+    }
+    else even = 0;
+    tot=2^numDisk-1;
+    for (k = 1; k < tot; k++){
+        if (even == 0) {
+            if (k % 3 == 1) {
+                //movimentolegal(A, C, *direcao);
+            }
+            if (k % 3 == 2) {
+                //movimentolegal(A, B, *direcao);
+            }
+            if (k % 3 == 0) {
+                //movimentolegal(B, C, *direcao);
+            }
+
+        } else if (even == 1) {
+            if (k % 3 == 1) {
+                //movimentolegal(A, B, *direcao);
+            }
+            if (k % 3 == 2) {
+                //movimentolegal(A, C, *direcao);
+            }
+            if (k % 3 == 0) {
+                //movimentolegal(C, B, *direcao);
+            }
+        }
+        printf("\n Mover disco de pino %d para o pino %d", de, para);
+        fprintf(arq, "\n Mover disco de pino %d para o pino %d", de, para);
+        (*seq)++;
+    }
+}
+
+void torreDeHanoiIterativa_metodo_Bit_a_Bit(int numDisk, int printar, int *seq, FILE *arq)
+{
+    //https://daemoniolabs.wordpress.com/2012/05/16/solucao-iterativa-para-o-problema-da-torre-de-hanoi/
+
+    int k;
+    int de, para;
+
+    if (printar == 1) {
+        for (k = 1; k < (1 << numDisk); k++) {  // " k < (1 << n)" equivale à 2^n-1
+            /* obtem haste origem */
+            de = (k & (k - 1)) % 3;
+
+            /* obtem haste destino */
+            para = ((k | (k - 1)) + 1) % 3;
+
+            /* descreve o movimento */
+            printf(      "\n Mover disco de pino %d para o pino %d", de, para);
+            fprintf(arq, "\n Mover disco de pino %d para o pino %d", de, para);
+            (*seq)++;
+        }
+    } else {
+        for (k = 1; k < (1 << numDisk); k++) {  // " k < (1 << n)" equivale à 2^n-1
+            /* obtem haste origem */
+            de = (k & (k - 1)) % 3;
+
+            /* obtem haste destino */
+            para = ((k | (k - 1)) + 1) % 3;
+            (*seq)++;
+        }
+    }
+
+}
+
 void torreDeHanoiRecursivaComPrint(int numDisk, char pinoInicial, char pinoDestino, char pinoAuxiliar, int *seq, FILE *arq)
 {
     (*seq)++;
@@ -153,30 +226,74 @@ int main()
     double s_CPU_inicial;
     double s_total_inicial;
     double s_total_final;
-    int operacao;
+    int operacao, metodo;
 
-    printf("Digite o valor da proxima operação: 1 - Gerar arquivo com comparação de movimentos | 2 - Gerar arquivo com comparação de tempo de processamento | 3 - Gerar arquivo com movimentos do disco entre os pinos\n");
+    printf("Digite o valor da proxima operação: \n"
+           "1 - Gerar arquivo com comparação de movimentos | \n"
+           "2 - Gerar arquivo com comparação de tempo de processamento | \n"
+           "3 - Gerar arquivo com movimentos do disco entre os pinos |\n"
+           "4 - Sair\n");
 
     scanf("%d", &operacao);
+
     while (operacao == 1 || operacao == 2 || operacao == 3)
     {
-        if (operacao == 1)
-        {
-            ns = 10;
-            FILE *novoArq = fopen("comparacoesRecursivaMovimentos.txt", "w");
-            while (ns >= 3)
-            {
-                printf("\nA sequencia de movimentos com N = %d é:\n", ns);
-                fprintf(novoArq, "\nA sequencia de movimentos com N = %d é:\n", ns);
-                torreDeHanoiRecursivaComPrint(ns, 'A', 'C', 'B', &seq, novoArq);
-                fprintf(novoArq, "\nA sequencia de passos na torre de hanoi com N = %d é: %d\n", ns, seq);
-                printf("\nA sequencia de passos na torre de hanoi é com N = %d  é: %d", ns, seq);
-                printf("\n");
-                seq = 0;
-                ns--;
+        printf("1 - Método Recursivo: \n"
+               "2 - Método Iterativo bit a bit | \n"
+               "3 - Método Iterativo xxxxxxxxx | \n"
+               "4 - Abortar\n");
+        scanf("%d", &metodo);
+
+        if (operacao == 1) {
+            if (metodo == 1) {
+                ns = 10;
+                FILE *novoArq = fopen("comparacoesRecursivaMovimentos.txt", "w");
+                while (ns >= 3) {
+                    printf("\nA sequencia de movimentos com N = %d é:\n", ns);
+                    fprintf(novoArq, "\nA sequencia de movimentos com N = %d é:\n", ns);
+                    torreDeHanoiRecursivaComPrint(ns, 'A', 'C', 'B', &seq, novoArq);
+                    fprintf(novoArq, "\nA sequencia de passos na torre de hanoi com N = %d é: %d\n", ns, seq);
+                    printf("\nA sequencia de passos na torre de hanoi é com N = %d  é: %d", ns, seq);
+                    printf("\n");
+                    seq = 0;
+                    ns--;
+                }
+                fclose(novoArq);
+                ns = 10;
             }
-            fclose(novoArq);
-            ns = 10;
+            if (metodo == 2) {
+                ns = 10;
+                FILE *novoArq = fopen("comparacoesIterativoBaBMovimentos.txt", "w");
+                while (ns >= 3) {
+                    printf("\nA sequencia de movimentos com N = %d é:\n", ns);
+                    fprintf(novoArq, "\nA sequencia de movimentos com N = %d é:\n", ns);
+                    torreDeHanoiIterativa_metodo_Bit_a_Bit(ns, 1, &seq, novoArq);
+                    fprintf(novoArq, "\nA sequencia de passos na torre de hanoi com N = %d é: %d\n", ns, seq);
+                    printf("\nA sequencia de passos na torre de hanoi é com N = %d  é: %d", ns, seq);
+                    printf("\n");
+                    seq = 0;
+                    ns--;
+                }
+                fclose(novoArq);
+                ns = 10;
+            }
+            if (metodo == 3) {
+                ns = 10;
+                FILE *novoArq = fopen("comparacoesIterativoxxxMovimentos.txt", "w");
+                while (ns >= 3) {
+                    printf("\nA sequencia de movimentos com N = %d é:\n", ns);
+                    fprintf(novoArq, "\nA sequencia de movimentos com N = %d é:\n", ns);
+                    torreDeHanoiIterativa_metodo_xxx(ns, 1, &seq, novoArq);
+                    fprintf(novoArq, "\nA sequencia de passos na torre de hanoi com N = %d é: %d\n", ns, seq);
+                    printf("\nA sequencia de passos na torre de hanoi é com N = %d  é: %d", ns, seq);
+                    printf("\n");
+                    seq = 0;
+                    ns--;
+                }
+                fclose(novoArq);
+                ns = 10;
+            }
+
         }
         else if (operacao == 3)
         {
@@ -206,26 +323,66 @@ int main()
             printarPinos(pinoOrigem, pinoDestino, pinoAuxiliar, novoArq3);
             fclose(novoArq3);
         }
-        else
+        else if (operacao == 2)
         {
-            FILE *novoArq2 = fopen("comparacoesRecursivaTempoProcessamento.txt", "w");
-            ns = 30;
-            while (ns >= 3)
-            {
-                Tempo_CPU_Sistema(&s_CPU_inicial, &s_total_inicial);
+            if (metodo == 1) {
+                FILE *novoArq2 = fopen("comparacoesRecursivaTempoProcessamento.txt", "w");
+                ns = 30;
+                while (ns >= 3) {
+                    Tempo_CPU_Sistema(&s_CPU_inicial, &s_total_inicial);
 
-                torreDeHanoiRecursiva(ns, 'A', 'C', 'B');
+                    torreDeHanoiRecursiva(ns, 'A', 'C', 'B');
 
-                Tempo_CPU_Sistema(&s_CPU_final, &s_total_final);
+                    Tempo_CPU_Sistema(&s_CPU_final, &s_total_final);
 
-                printf("\nTempo de CPU total para N = %d é %f\n", ns, s_CPU_final - s_CPU_inicial);
-                fprintf(novoArq2, "\nTempo de CPU total para N = %d é %f\n", ns, s_CPU_final - s_CPU_inicial);
-                ns--;
+                    printf("\nTempo de CPU total para N = %d é %f\n", ns, s_CPU_final - s_CPU_inicial);
+                    fprintf(novoArq2, "\nTempo de CPU total para N = %d é %f\n", ns, s_CPU_final - s_CPU_inicial);
+                    ns--;
+                }
+                fclose(novoArq2);
+                ns = 30;
             }
-            fclose(novoArq2);
-            ns = 30;
+            if (metodo == 2) {
+                FILE *novoArq2 = fopen("comparacoesIterativaBaBTempoProcessamento.txt", "w");
+                ns = 30;
+                while (ns >= 3) {
+                    Tempo_CPU_Sistema(&s_CPU_inicial, &s_total_inicial);
+
+                    torreDeHanoiIterativa_metodo_Bit_a_Bit(ns, 0, &seq, novoArq2);
+
+                    Tempo_CPU_Sistema(&s_CPU_final, &s_total_final);
+
+                    printf("\nTempo de CPU total para N = %d é %f\n", ns, s_CPU_final - s_CPU_inicial);
+                    fprintf(novoArq2, "\nTempo de CPU total para N = %d é %f\n", ns, s_CPU_final - s_CPU_inicial);
+                    ns--;
+                }
+                fclose(novoArq2);
+                ns = 30;
+            }
+            if (metodo == 3) {
+                FILE *novoArq2 = fopen("comparacoesIterativaxxxTempoProcessamento.txt", "w");
+                ns = 30;
+                while (ns >= 3) {
+                    Tempo_CPU_Sistema(&s_CPU_inicial, &s_total_inicial);
+
+                    torreDeHanoiIterativa_metodo_xxx(ns, 0, &seq, novoArq2);
+
+                    Tempo_CPU_Sistema(&s_CPU_final, &s_total_final);
+
+                    printf("\nTempo de CPU total para N = %d é %f\n", ns, s_CPU_final - s_CPU_inicial);
+                    fprintf(novoArq2, "\nTempo de CPU total para N = %d é %f\n", ns, s_CPU_final - s_CPU_inicial);
+                    ns--;
+                }
+                fclose(novoArq2);
+                ns = 30;
+            }
         }
-        printf("Digite o valor da proxima operação: 1 - Gerar arquivo com comparação de movimentos | 2 - Gerar arquivo com comparação de tempo de processamento | 3 - Gerar arquivo com movimentos do disco entre os pinos\n");
+        printf(" \n \n");
+        printf("Digite o valor da proxima operação: \n"
+               "1 - Gerar arquivo com comparação de movimentos | \n"
+               "2 - Gerar arquivo com comparação de tempo de processamento | \n"
+               "3 - Gerar arquivo com movimentos do disco entre os pinos |\n"
+               "4 - Sair\n");
 
         scanf("%d", &operacao);
         seq = 0;
